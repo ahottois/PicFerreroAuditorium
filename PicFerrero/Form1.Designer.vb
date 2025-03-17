e3839105f40b9c03662f56103f6d71fd0e0884f3
ahott <ahott@LALO>
2025-03-17 20:30:15 +0100

    Ajoutez des fichiers projet.

---
+++ PicFerrero/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(296, 234)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 19)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(127, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Keep the picture ratio"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(16, 19)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(32, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Height :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(148, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Width :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(57, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(195, 26)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(85, 20)
+        Me.TextBox2.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 305)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(296, 59)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Size of one picture"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 252)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(296, 47)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "How many pictures do we put one after the other one"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 508)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(296, 50)
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.CheckBox2)
+        Me.GroupBox3.Controls.Add(Me.CheckBox1)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 370)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(296, 100)
+        Me.GroupBox3.TabIndex = 12
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Options"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 42)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(106, 17)
+        Me.CheckBox2.TabIndex = 13
+        Me.CheckBox2.Text = "Zoom the picture"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 479)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Load Picture"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(161, 479)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(147, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Save Picture"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(315, 570)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Ferrero Auditorium Picture Resizer"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ PicFerrero/Form1.vb
@@ -0,0 +1,365 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Xml
+
+Public Class Form1
+    Private originalImage As Image = Nothing
+    Private lastUsedDirectory As String = ""
+    Private settingsFilePath As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize settings path
+        Dim appDataFolder As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Ferrero\hottois\picture")
+
+        ' Create directory if it doesn't exist
+        If Not Directory.Exists(appDataFolder) Then
+            Directory.CreateDirectory(appDataFolder)
+        End If
+
+        settingsFilePath = Path.Combine(appDataFolder, "settings.xml")
+
+        ' Load settings if the file exists
+        LoadSettings()
+
+        ' Disable save button initially (until an image is loaded)
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Save settings before closing the application
+        SaveSettings()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Load Picture button click handler
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp"
+        openFileDialog.Title = "Select an Image"
+
+        ' Use the last used directory if available
+        If Not String.IsNullOrEmpty(lastUsedDirectory) AndAlso Directory.Exists(lastUsedDirectory) Then
+            openFileDialog.InitialDirectory = lastUsedDirectory
+        End If
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                ' Load the selected image
+                originalImage = Image.FromFile(openFileDialog.FileName)
+                PictureBox1.Image = originalImage
+
+                ' Enable the Save button
+                Button2.Enabled = True
+
+                ' Remember the directory for next time
+                lastUsedDirectory = Path.GetDirectoryName(openFileDialog.FileName)
+
+                ' Set default values for width and height if not already set
+                If String.IsNullOrEmpty(TextBox1.Text) Then
+                    TextBox1.Text = originalImage.Height.ToString()
+                End If
+
+                If String.IsNullOrEmpty(TextBox2.Text) Then
+                    TextBox2.Text = originalImage.Width.ToString()
+                End If
+
+                ' Update preview
+                UpdatePreview()
+            Catch ex As Exception
+                MessageBox.Show("Error loading image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Save Picture button click handler
+        If originalImage Is Nothing Then
+            MessageBox.Show("Please load an image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png|BMP Image|*.bmp"
+        saveFileDialog.Title = "Save the Modified Image"
+        saveFileDialog.DefaultExt = "jpg"
+
+        ' Use the last used directory if available
+        If Not String.IsNullOrEmpty(lastUsedDirectory) AndAlso Directory.Exists(lastUsedDirectory) Then
+            saveFileDialog.InitialDirectory = lastUsedDirectory
+        End If
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                ' Create and save the modified image
+                Dim finalImage As Image = CreateFinalImage()
+
+                ' Save with the appropriate format based on file extension
+                Dim format As ImageFormat = ImageFormat.Jpeg
+                Select Case Path.GetExtension(saveFileDialog.FileName).ToLower()
+                    Case ".png"
+                        format = ImageFormat.Png
+                    Case ".bmp"
+                        format = ImageFormat.Bmp
+                    Case Else
+                        format = ImageFormat.Jpeg
+                End Select
+
+                finalImage.Save(saveFileDialog.FileName, format)
+
+                ' Remember the directory for next time
+                lastUsedDirectory = Path.GetDirectoryName(saveFileDialog.FileName)
+
+                ' Save settings
+                SaveSettings()
+
+                MessageBox.Show("Image saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error saving image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Function CreateFinalImage() As Image
+        ' Get parameters from controls
+        Dim count As Integer = CInt(NumericUpDown1.Value)
+        Dim singleHeight, singleWidth As Integer
+
+        If Not Integer.TryParse(TextBox1.Text, singleHeight) OrElse singleHeight <= 0 Then
+            singleHeight = originalImage.Height
+        End If
+
+        If Not Integer.TryParse(TextBox2.Text, singleWidth) OrElse singleWidth <= 0 Then
+            singleWidth = originalImage.Width
+        End If
+
+        ' Adjust dimensions if keep ratio is checked
+        If CheckBox1.Checked Then
+            Dim originalRatio As Double = CDbl(originalImage.Width) / CDbl(originalImage.Height)
+
+            ' If height was changed, adjust width
+            If TextBox1.Modified AndAlso Not TextBox2.Modified Then
+                singleWidth = CInt(singleHeight * originalRatio)
+            Else ' Otherwise adjust height based on width
+                singleHeight = CInt(singleWidth / originalRatio)
+            End If
+        End If
+
+        ' Create a new bitmap with the calculated dimensions
+        Dim totalWidth As Integer = singleWidth * count
+        Dim resultImage As New Bitmap(totalWidth, singleHeight)
+
+        Using g As Graphics = Graphics.FromImage(resultImage)
+            g.Clear(Color.White) ' Set background color
+
+            ' Draw the image the specified number of times
+            For i As Integer = 0 To count - 1
+                Dim destRect As New Rectangle(i * singleWidth, 0, singleWidth, singleHeight)
+
+                ' Determine the source rectangle based on zoom option
+                Dim srcRect As Rectangle
+                If CheckBox2.Checked Then
+                    ' Zoom: use the entire image stretched to fit the destination
+                    srcRect = New Rectangle(0, 0, originalImage.Width, originalImage.Height)
+                Else
+                    ' No zoom: preserve aspect ratio and center
+                    Dim srcWidth, srcHeight As Integer
+                    Dim srcX, srcY As Integer
+
+                    Dim originalRatio As Double = CDbl(originalImage.Width) / CDbl(originalImage.Height)
+                    Dim destRatio As Double = CDbl(singleWidth) / CDbl(singleHeight)
+
+                    If originalRatio > destRatio Then
+                        ' Original is wider
+                        srcHeight = originalImage.Height
+                        srcWidth = CInt(srcHeight * destRatio)
+                        srcX = (originalImage.Width - srcWidth) \ 2
+                        srcY = 0
+                    Else
+                        ' Original is taller
+                        srcWidth = originalImage.Width
+                        srcHeight = CInt(srcWidth / destRatio)
+                        srcX = 0
+                        srcY = (originalImage.Height - srcHeight) \ 2
+                    End If
+
+                    srcRect = New Rectangle(srcX, srcY, srcWidth, srcHeight)
+                End If
+
+                ' Draw the image
+                g.DrawImage(originalImage, destRect, srcRect, GraphicsUnit.Pixel)
+            Next
+        End Using
+
+        Return resultImage
+    End Function
+
+    Private Sub UpdatePreview()
+        ' Update the preview whenever settings change
+        If originalImage IsNot Nothing Then
+            Try
+                Dim finalImage As Image = CreateFinalImage()
+
+                ' Show preview in PictureBox2
+                If PictureBox2.Image IsNot Nothing Then
+                    PictureBox2.Image.Dispose()
+                End If
+
+                PictureBox2.Image = finalImage
+            Catch ex As Exception
+                ' Handle preview errors silently
+            End Try
+        End If
+    End Sub
+
+    ' Save application settings to XML file
+    Private Sub SaveSettings()
+        Try
+            ' Create XML document
+            Dim settings As New XmlDocument()
+            Dim declaration As XmlDeclaration = settings.CreateXmlDeclaration("1.0", "UTF-8", Nothing)
+            settings.AppendChild(declaration)
+
+            ' Create root element
+            Dim root As XmlElement = settings.CreateElement("Settings")
+            settings.AppendChild(root)
+
+            ' Add last used directory
+            Dim dirElement As XmlElement = settings.CreateElement("LastDirectory")
+            dirElement.InnerText = lastUsedDirectory
+            root.AppendChild(dirElement)
+
+            ' Add number of copies
+            Dim countElement As XmlElement = settings.CreateElement("CopyCount")
+            countElement.InnerText = NumericUpDown1.Value.ToString()
+            root.AppendChild(countElement)
+
+            ' Add height
+            Dim heightElement As XmlElement = settings.CreateElement("Height")
+            heightElement.InnerText = TextBox1.Text
+            root.AppendChild(heightElement)
+
+            ' Add width
+            Dim widthElement As XmlElement = settings.CreateElement("Width")
+            widthElement.InnerText = TextBox2.Text
+            root.AppendChild(widthElement)
+
+            ' Add keep ratio setting
+            Dim ratioElement As XmlElement = settings.CreateElement("KeepRatio")
+            ratioElement.InnerText = CheckBox1.Checked.ToString()
+            root.AppendChild(ratioElement)
+
+            ' Add zoom setting
+            Dim zoomElement As XmlElement = settings.CreateElement("ZoomPicture")
+            zoomElement.InnerText = CheckBox2.Checked.ToString()
+            root.AppendChild(zoomElement)
+
+            ' Save the document
+            settings.Save(settingsFilePath)
+        Catch ex As Exception
+            ' Handle silently - it's not critical if we can't save settings
+            Debug.WriteLine("Error saving settings: " & ex.Message)
+        End Try
+    End Sub
+
+    ' Load application settings from XML file
+    Private Sub LoadSettings()
+        Try
+            If File.Exists(settingsFilePath) Then
+                Dim settings As New XmlDocument()
+                settings.Load(settingsFilePath)
+
+                ' Get last used directory
+                Dim dirNode As XmlNode = settings.SelectSingleNode("//LastDirectory")
+                If dirNode IsNot Nothing Then
+                    lastUsedDirectory = dirNode.InnerText
+                End If
+
+                ' Get number of copies
+                Dim countNode As XmlNode = settings.SelectSingleNode("//CopyCount")
+                If countNode IsNot Nothing AndAlso Not String.IsNullOrEmpty(countNode.InnerText) Then
+                    Dim count As Integer
+                    If Integer.TryParse(countNode.InnerText, count) Then
+                        NumericUpDown1.Value = Math.Max(NumericUpDown1.Minimum, Math.Min(NumericUpDown1.Maximum, count))
+                    End If
+                End If
+
+                ' Get height
+                Dim heightNode As XmlNode = settings.SelectSingleNode("//Height")
+                If heightNode IsNot Nothing Then
+                    TextBox1.Text = heightNode.InnerText
+                End If
+
+                ' Get width
+                Dim widthNode As XmlNode = settings.SelectSingleNode("//Width")
+                If widthNode IsNot Nothing Then
+                    TextBox2.Text = widthNode.InnerText
+                End If
+
+                ' Get keep ratio setting
+                Dim ratioNode As XmlNode = settings.SelectSingleNode("//KeepRatio")
+                If ratioNode IsNot Nothing AndAlso Not String.IsNullOrEmpty(ratioNode.InnerText) Then
+                    Dim keepRatio As Boolean
+                    If Boolean.TryParse(ratioNode.InnerText, keepRatio) Then
+                        CheckBox1.Checked = keepRatio
+                    End If
+                End If
+
+                ' Get zoom setting
+                Dim zoomNode As XmlNode = settings.SelectSingleNode("//ZoomPicture")
+                If zoomNode IsNot Nothing AndAlso Not String.IsNullOrEmpty(zoomNode.InnerText) Then
+                    Dim zoomPicture As Boolean
+                    If Boolean.TryParse(zoomNode.InnerText, zoomPicture) Then
+                        CheckBox2.Checked = zoomPicture
+                    End If
+                End If
+            End If
+        Catch ex As Exception
+            ' Handle silently - it's not critical if we can't load settings
+            Debug.WriteLine("Error loading settings: " & ex.Message)
+        End Try
+    End Sub
+
+    ' Event handlers for control changes
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        UpdatePreview()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If CheckBox1.Checked AndAlso originalImage IsNot Nothing AndAlso Integer.TryParse(TextBox1.Text, Nothing) Then
+            Dim height As Integer = Integer.Parse(TextBox1.Text)
+            Dim originalRatio As Double = CDbl(originalImage.Width) / CDbl(originalImage.Height)
+
+            ' Update width to maintain ratio
+            TextBox2.Text = CInt(height * originalRatio).ToString()
+        End If
+        UpdatePreview()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If CheckBox1.Checked AndAlso originalImage IsNot Nothing AndAlso Integer.TryParse(TextBox2.Text, Nothing) Then
+            Dim width As Integer = Integer.Parse(TextBox2.Text)
+            Dim originalRatio As Double = CDbl(originalImage.Width) / CDbl(originalImage.Height)
+
+            ' Update height to maintain ratio
+            TextBox1.Text = CInt(width / originalRatio).ToString()
+        End If
+        UpdatePreview()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        ' When "keep ratio" is toggled, update dimensions
+        If CheckBox1.Checked AndAlso originalImage IsNot Nothing Then
+            Dim originalRatio As Double = CDbl(originalImage.Width) / CDbl(originalImage.Height)
+
+            ' Update width based on current height
+            If Integer.TryParse(TextBox1.Text, Nothing) Then
+                Dim height As Integer = Integer.Parse(TextBox1.Text)
+                TextBox2.Text = CInt(height * originalRatio).ToString()
+            End If
+        End If
+        UpdatePreview()
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        UpdatePreview()
+    End Sub
+End Class
